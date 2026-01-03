eab9126422843a5ccbcc278c3607cba80e67f110
Silen Locatelli <silen.locatelli@gmx.ch>
2026-01-03 14:01:24 +0100

    feat:fake message

--- src/chat/handler.rs
+++ src/chat/handler.rs
@@ -1,7 +1,7 @@
 use crate::chat::{ChatRequest, ChatResponse};
 
 pub struct Context {
-    session_id: String,
+    _session_id: String,
     service_name: String,
     tenant: String,
     knowledge_base: Vec<String>,
@@ -15,7 +15,7 @@ impl Context {
         knowledge_base: Vec<String>,
     ) -> Self {
         Context {
-            session_id: session_id.to_owned(),
+            _session_id: session_id.to_owned(),
             service_name: service_name.to_owned(),
             tenant: tenant.to_owned(),
             knowledge_base,
@@ -32,14 +32,11 @@ pub fn message(r: ChatRequest, context: &Context, fake: bool) -> ChatResponse {
         .find(|msg| msg.role.as_deref() == Some("user") || msg.role.is_none())
         .and_then(|msg| msg.text.clone());
 
-    let response = if fake {
+    if fake {
         fake_message(context, latest_message)
     } else {
-        log::error!("Not yet implemented");
-        panic!("Not yet implemented")
-    };
-
-    response
+        real_message(context, latest_message)
+    }
 }
 
 pub fn fake_message(context: &Context, msg: Option<String>) -> ChatResponse {
@@ -52,4 +49,35 @@ pub fn fake_message(context: &Context, msg: Option<String>) -> ChatResponse {
         text: res,
         error: None,
     }
+}
+
+pub fn real_message(context: &Context, msg: Option<String>) -> ChatResponse {
+    let context_with_prios: String = context
+        .knowledge_base
+        .iter()
+        .enumerate()
+        .map(|(i, k)| format!("Priority: {i} Context: {}", k))
+        .collect::<Vec<String>>()
+        .join(", ");
+
+    // todo actually send to llm
+    let _send_to_llm = match msg {
+        Some(msg) => {
+            format!(
+                "The user sent: {msg}, answer it with this context: {}, you are {} AI responding to your message in tenant '{}'",
+                context_with_prios, context.service_name, context.tenant
+            )
+        }
+        None => {
+            format!(
+                "No message provided, try with this context: {}, you are {} AI responding to your message in tenant '{}'",
+                context_with_prios, context.service_name, context.tenant
+            )
+        }
+    };
+
+    // todo send to llm
+
+    // todo replace with real message
+    fake_message(context, None)
 }
